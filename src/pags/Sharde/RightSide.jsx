9fc5de757113bdcb1915f8b8263d099ca6dcbf86
rhsabbir1 <rhsabbir090@gmai.com>
2023-04-30 10:44:24 +0600

    icon and find us social done

--- src/pags/Sharde/RightSide.jsx
+++ src/pags/Sharde/RightSide.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
-import { FaGoogle , FaGithub } from 'react-icons/fa';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub, FaFacebook,FaTwitter , FaInstagram } from 'react-icons/fa';
 
 const RightSide = () => {
     return (
         <div>
             <h4>Login With</h4>
-            <Button className='mb-2' variant="outline-primary"> <FaGoogle /> Login with Google</Button>{' '}
-            <Button variant="outline-secondary"> <FaGithub />Login with Git Hub</Button>{' '}
+            <Button className='mb-2' variant="outline-primary"> <FaGoogle /> Login with Google</Button>
+            <Button variant="outline-secondary"> <FaGithub />Login with Git Hub</Button>
+            <div className='mt-4'>
+                <h4>Find us on</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook></FaFacebook> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram></FaInstagram> Instagram</ListGroup.Item>
+                    
+                </ListGroup>
+            </div>
         </div>
     );
 };
